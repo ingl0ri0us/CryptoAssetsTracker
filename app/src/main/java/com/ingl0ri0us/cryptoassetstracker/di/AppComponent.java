2556356f4d8602d23304faa8b89c0028321bb2bd
ingl0ri0us <rinat.y@protonmail.com>
2020-01-19 11:05:56 +0300

    app module

---
+++ app/src/main/java/com/ingl0ri0us/cryptoassetstracker/di/AppComponent.java
@@ -0,0 +1,13 @@
+package com.ingl0ri0us.cryptoassetstracker.di;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {
+        AppModule.class
+})
+public interface AppComponent {
+
+}
